b1aeec788b0867ea062afb5ca2cc9e96599b1c55
Ryan Santos <rsantos@weareenvoy.com>
2024-09-15 12:14:20 -0700

    Fix mouse follower limitation issue

--- app/hooks/use-follow-pointer.ts
+++ app/hooks/use-follow-pointer.ts
@@ -2,9 +2,9 @@ import { RefObject, useEffect } from 'react';
 import { useMotionValue, useSpring, frame, type Spring } from 'framer-motion';
 
 const spring: Partial<Spring> = {
-	// damping: 10,
-	// stiffness: 50,
-	// restDelta: 0.001,
+	damping: 15,
+	stiffness: 100,
+	restDelta: 0.001,
 };
 
 export function useFollowPointer(ref: RefObject<HTMLElement>) {
@@ -15,19 +15,21 @@ export function useFollowPointer(ref: RefObject<HTMLElement>) {
 
 	useEffect(() => {
 		if (!ref.current) return;
+		if (window?.innerWidth < 992) return;
 
-		const handlePointerMove = ({ clientX, clientY }: MouseEvent) => {
+		const handlePointerMove = ({ pageX, pageY }: MouseEvent) => {
 			const element = ref.current!;
 
 			frame.read(() => {
-				xPoint.set(clientX - element.offsetLeft - element.offsetWidth / 2);
-				yPoint.set(clientY - element.offsetTop - element.offsetHeight / 2);
+				xPoint.set(pageX - element.offsetLeft - element.offsetWidth / 2);
+				yPoint.set(pageY - element.offsetTop - element.offsetHeight / 2);
 			});
 		};
 
 		window.addEventListener('pointermove', handlePointerMove);
 
 		return () => window.removeEventListener('pointermove', handlePointerMove);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return { x, y };
--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 import {
 	Links,
 	Meta,
@@ -7,14 +7,14 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from '@remix-run/react';
-import Header from './components/header';
-import Sidebar from './components/sidebar';
-import { useFollowPointer } from './hooks/use-follow-pointer';
+import Header from '~/components/header';
+import Sidebar from '~/components/sidebar';
+import { useFollowPointer } from '~/hooks/use-follow-pointer';
 
 export default function App() {
 	const [sidebarOpen, setSidebarOpen] = useState(false);
-	const mouseFollowerRef = useRef<HTMLDivElement>(null);
-	const { x, y } = useFollowPointer(mouseFollowerRef);
+	const pointerFollowerRef = useRef<HTMLDivElement>(null);
+	const { x, y } = useFollowPointer(pointerFollowerRef);
 
 	function toggleSidebar(state: boolean) {
 		if (state === true || state === false) {
@@ -37,14 +37,20 @@ export default function App() {
 					<Header sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
 					<Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
 					<Outlet />
+					<motion.div
+						ref={pointerFollowerRef}
+						style={{
+							x,
+							y,
+							borderRadius: '50%',
+							height: 10,
+							width: 10,
+							backgroundColor: 'white',
+						}}
+					/>
 				</main>
 				<ScrollRestoration />
 				<Scripts />
-				<motion.div
-					className="mouse-follower"
-					ref={mouseFollowerRef}
-					style={{ x, y }}
-				/>
 			</body>
 		</html>
 	);
