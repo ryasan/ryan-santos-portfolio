04de6ebe92265a94226d4816498ade29f14cdcd4
Ryan Santos <rsantos@weareenvoy.com>
2024-09-15 19:19:27 -0700

    Update gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { netlifyPlugin } from '@netlify/remix-edge-adapter/plugin';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [
 		remix(),
 		netlifyPlugin(),
 		tsconfigPaths(),
-		// viteStaticCopy({
-		// 	targets: [
-		// 		{
-		// 			src: 'app/assets/*',
-		// 			dest: 'assets',
-		// 		},
-		// 	],
-		// }),
 	],
 });
