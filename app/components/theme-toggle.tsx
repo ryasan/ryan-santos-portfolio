f5e7461be38dc0df805856a39808f7e09b42273f
Ryan Santos <ryansantos86@gmail.com>
2025-09-23 22:43:53 -0700

    Refactor: Remove unused ThemeToggle component code

--- app/components/theme-toggle.tsx
+++ app/components/theme-toggle.tsx
@@ -40,3 +40,53 @@ function ThemeToggle({ darkImage = '', lightImage = '' }: ThemeToggleProps) {
 }
 
 export default ThemeToggle
+
+// function ThemeToggle({
+// 	userPreference,
+// }: {
+// 	userPreference?: Theme | null
+// }) {
+// 	const fetcher = useFetcher<typeof action>()
+
+// 	const [form] = useForm({
+// 		id: 'theme-toggle',
+// 		lastResult: fetcher.data?.result as any,
+// 	})
+
+// 	const mode = userPreference ?? 'system'
+// 	const nextMode =
+// 		mode === 'system' ? 'light' : mode === 'light' ? 'dark' : 'system'
+// 	const modeLabel = {
+// 		light: (
+// 			<div>
+// 				<Image src="/public/images/sun.svg" alt="Light" />
+// 				<span className="sr-only">Light</span>
+// 			</div>
+// 		),
+// 		dark: (
+// 			<div>
+// 				<Image src="/public/images/moon.svg" alt="Dark" />
+// 				<span className="sr-only">Dark</span>
+// 			</div>
+// 		),
+// 		system: (
+// 			<div>
+// 				<Image src="/public/images/laptop.svg" alt="System" />
+// 				<span className="sr-only">System</span>
+// 			</div>
+// 		),
+// 	}
+
+// 	return (
+// 		<fetcher.Form
+// 			method="POST"
+// 			{...getFormProps(form)}
+// 			action="/resources/theme-toggle"
+// 		>
+// 			<input type="hidden" name="theme" value={nextMode} />
+// 			<div>
+// 				<button type="submit">{modeLabel[mode]}</button>
+// 			</div>
+// 		</fetcher.Form>
+// 	)
+// }
