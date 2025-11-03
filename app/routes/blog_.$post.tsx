63d45210bbe82dd269db54b5d034cb36d19256d1
Ryan Santos <ryansantos86@gmail.com>
2025-11-03 14:59:42 -0800

    Feat: Add meta function for blog post SEO

--- app/routes/blog_.$post.tsx
+++ app/routes/blog_.$post.tsx
@@ -1,6 +1,6 @@
 import BlogPostSection from '~/components/sections/blog-post-section'
 import { client } from '~/services/contentful.server'
-import { json, LoaderFunctionArgs } from '@remix-run/node'
+import { json, LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -11,6 +11,30 @@ export async function loader({ params }: LoaderFunctionArgs) {
 
 	const blog = await client.getBlogBySlug(post)
 	return json({ blog })
+}
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+	if (!data?.blog) {
+		return [{ title: 'Page Not Found' }]
+	}
+
+	const { blog } = data
+
+	return [
+		{ title: blog.title },
+		{
+			name: 'description',
+			content: blog.description,
+		},
+		...(blog.openGraphImage
+			? [
+					{
+						property: 'og:image',
+						content: blog.openGraphImage.url,
+					},
+				]
+			: []),
+	]
 }
 
 export default function BlogPost() {
