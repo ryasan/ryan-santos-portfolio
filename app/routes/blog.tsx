f25170692d0c0e7943b938dade1e643eb6a55966
Ryan Santos <ryansantos86@gmail.com>
2025-11-03 12:44:48 -0800

    Refactor: Update blog page structure and logic

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,80 +1,49 @@
-// import BlogSection from '~/components/sections/blog-section'
-// import SectionRenderer from '~/components/section-renderer'
-// import type { MetaFunction } from '@netlify/remix-runtime'
-// import type { PagePageSectionsItem } from '~/graphql/__generated/sdk'
-// import { BlogFilterProvider } from '~/contexts/blog-filter-context'
-// import { client } from '~/services/contentful.server'
-// import { json } from '@remix-run/server-runtime'
-// import { useLoaderData } from '@remix-run/react'
-
-// export async function loader() {
-// 	const page = await client.getPageBySlug('blog')
-// 	const blogs = await client.getAllBlogs('publishDate_DESC')
-
-// 	if (!page) {
-// 		throw new Response('Not Found', { status: 404 })
-// 	}
-
-// 	return json({ page, blogs })
-// }
-
-// export const meta: MetaFunction<typeof loader> = ({ data }) => {
-// 	if (!data?.page) {
-// 		return [{ title: 'Page Not Found' }]
-// 	}
-
-// 	const { page } = data
-
-// 	return [
-// 		{ title: page.seoMetadata?.title || page.title },
-// 		{
-// 			name: 'description',
-// 			content: page.seoMetadata?.description || 'Page description',
-// 		},
-// 		...(page.seoMetadata?.ogImage
-// 			? [
-// 					{
-// 						property: 'og:image',
-// 						content: page.seoMetadata.ogImage.url,
-// 					},
-// 				]
-// 			: []),
-// 	]
-// }
-
-// export default function BlogPage() {
-// 	const { page, blogs } = useLoaderData<typeof loader>()
-
-// 	return (
-// 		<BlogFilterProvider initialPosts={blogs}>
-// 			{page.pageSectionsCollection?.items?.map(
-// 				(section: PagePageSectionsItem) => {
-// 					if (!section?.sys?.id) return null
-// 					return <SectionRenderer key={section.sys.id} section={section} />
-// 				},
-// 			)}
-// 			<BlogSection />
-// 		</BlogFilterProvider>
-// 	)
-// }
-
-import BlogPostSection from '~/components/sections/blog-post-section'
+import BlogSection from '~/components/sections/blog-section'
+import SectionRenderer from '~/components/section-renderer'
+import type { MetaFunction } from '@netlify/remix-runtime'
+import type { PagePageSectionsItem } from '~/graphql/__generated/sdk'
+import { BlogFilterProvider } from '~/contexts/blog-filter-context'
 import { client } from '~/services/contentful.server'
-import { json, LoaderFunctionArgs } from '@remix-run/node'
+import { json } from '@remix-run/server-runtime'
 import { useLoaderData } from '@remix-run/react'
 
-export async function loader({ params }: LoaderFunctionArgs) {
-	// const { post } = params
-	// if (!post) {
-	// 	throw new Response('Not Found', { status: 404 })
-	// }
+export async function loader() {
+	const page = await client.getPageBySlug('blog')
+	const blogs = await client.getAllBlogs('publishDate_DESC')
 
-	const blog = await client.getBlogBySlug('typescript-type-guards')
-	return json({ blog })
+	if (!page) {
+		throw new Response('Not Found', { status: 404 })
+	}
+
+	return json({ page, blogs })
 }
 
-export default function BlogPost() {
-	const { blog } = useLoaderData<typeof loader>()
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+	if (!data?.page) {
+		return [{ title: 'Page Not Found' }]
+	}
 
-	return <BlogPostSection data={blog} />
+	const { page } = data
+
+	return [
+		{ title: page.seoMetadata?.title || page.title },
+		{
+			name: 'description',
+			content: page.seoMetadata?.description || 'Page description',
+		},
+		...(page.seoMetadata?.ogImage
+			? [
+					{
+						property: 'og:image',
+						content: page.seoMetadata.ogImage.url,
+					},
+				]
+			: []),
+	]
+}
+
+export default function BlogPage() {
+	// const { page, blogs } = useLoaderData<typeof loader>()
+
+	return <h1>Blog</h1>
 }
