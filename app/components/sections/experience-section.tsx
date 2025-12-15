58c8798bbede06f4a43bb0a95ac9cc0e3744eb16
Ryan Santos <ryansantos86@gmail.com>
2025-12-15 14:58:42 -0800

    Style: Update title size and margin in experience section

--- app/components/sections/experience-section.tsx
+++ app/components/sections/experience-section.tsx
@@ -142,7 +142,7 @@ export default function ExperienceSection({
 			<div className="container">
 				<div className={styles.box}>
 					{data?.title && (
-						<h2 className={clsx(styles.title, 'h1')} ref={titleRef}>
+						<h2 className={clsx(styles.title, 'h2')} ref={titleRef}>
 							{data?.title}
 						</h2>
 					)}
