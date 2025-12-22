2c3d02206371eeea6f4ed9f9c6c58f2b4128e47d
Ryan Santos <ryansantos86@gmail.com>
2025-12-22 08:53:29 -0800

    Refactor: Remove outdated sitemap tags that don't add value

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -1,5 +1,6 @@
-import { type LoaderFunctionArgs } from '@remix-run/node'
 import { client } from '~/services/contentful.server'
+import { type Blog, type Page } from '~/graphql/__generated/sdk'
+import { type LoaderFunctionArgs } from '@remix-run/node'
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
 	const url = new URL(request.url)
@@ -15,29 +16,27 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${pages
-		.map((page: any) => {
+		.map((page: Page) => {
 			const slug = page.slug
 			const lastMod = page.sys?.publishedAt
 				? new Date(page.sys.publishedAt).toISOString()
 				: new Date().toISOString()
-			
-            let pageUrl = `${baseUrl}/${slug}`
-            if (slug === 'home') {
-                pageUrl = `${baseUrl}/`
-            }
+
+			let pageUrl = `${baseUrl}/${slug}`
+			if (slug === 'home') {
+				pageUrl = `${baseUrl}/`
+			}
 
 			return `
     <url>
       <loc>${pageUrl}</loc>
       <lastmod>${lastMod}</lastmod>
-      <changefreq>weekly</changefreq>
-      <priority>${slug === 'home' ? '1.0' : '0.8'}</priority>
     </url>
   `
 		})
 		.join('')}
   ${blogs
-		.map((blog: any) => {
+		.map((blog: Blog) => {
 			const slug = blog.slug
 			const lastMod = blog.publishDate
 				? new Date(blog.publishDate).toISOString()
@@ -47,8 +46,6 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     <url>
       <loc>${baseUrl}/blog/${slug}</loc>
       <lastmod>${lastMod}</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
     </url>
   `
 		})
