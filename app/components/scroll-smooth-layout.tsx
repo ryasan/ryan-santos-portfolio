2c637cf34adf216273b3332cd65da2aaa961f279
Ryan Santos <rsantos@weareenvoy.com>
2026-02-04 16:25:55 -0800

    Feat: Reset layout to start of content on page change

--- app/components/scroll-smooth-layout.tsx
+++ app/components/scroll-smooth-layout.tsx
@@ -1,5 +1,7 @@
 import { ScrollSmoother } from 'gsap/ScrollSmoother'
 import { useGSAP } from '@gsap/react'
+import { useLocation } from '@remix-run/react'
+import { useLayoutEffect } from 'react'
 
 type ScrollSmoothLayoutProps = {
 	children: React.ReactNode
@@ -8,6 +10,8 @@ type ScrollSmoothLayoutProps = {
 export default function ScrollSmoothLayout({
 	children,
 }: ScrollSmoothLayoutProps) {
+	const location = useLocation()
+
 	useGSAP(() => {
 		// Only enable on desktop (non-touch devices)
 		const isTouchDevice =
@@ -26,6 +30,15 @@ export default function ScrollSmoothLayout({
 			instance.kill()
 		}
 	}, [])
+
+	useLayoutEffect(() => {
+		const smoother = ScrollSmoother.get()
+		if (smoother) {
+			smoother.scrollTop(0)
+		} else {
+			window.scrollTo(0, 0)
+		}
+	}, [location.pathname])
 
 	return (
 		<div id="smooth-wrapper">
