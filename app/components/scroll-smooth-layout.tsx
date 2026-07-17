136453100b1671d4cf6e75104e15e7553d7f0d3c
ryasan <ryansantos86@gmail.com>
2026-07-16 21:45:05 -0700

    Refactor: Comment out unused location logic

--- app/components/scroll-smooth-layout.tsx
+++ app/components/scroll-smooth-layout.tsx
@@ -1,7 +1,7 @@
 import { ScrollSmoother } from 'gsap/ScrollSmoother'
 import { useGSAP } from '@gsap/react'
-import { useLocation } from '@remix-run/react'
-import { useLayoutEffect } from 'react'
+// import { useLocation } from '@remix-run/react'
+// import { useLayoutEffect } from 'react'
 
 type ScrollSmoothLayoutProps = {
 	children: React.ReactNode
@@ -10,7 +10,7 @@ type ScrollSmoothLayoutProps = {
 export default function ScrollSmoothLayout({
 	children,
 }: ScrollSmoothLayoutProps) {
-	const location = useLocation()
+	// const location = useLocation()
 
 	useGSAP(() => {
 		// Only enable on desktop (non-touch devices)
@@ -30,15 +30,15 @@ export default function ScrollSmoothLayout({
 		}
 	}, [])
 
-	useLayoutEffect(() => {
-		// Reset scroll position to top on page change
-		const smoother = ScrollSmoother.get()
-		if (smoother) {
-			smoother.scrollTop(0)
-		} else {
-			window.scrollTo(0, 0)
-		}
-	}, [location.pathname])
+	// useLayoutEffect(() => {
+	// 	// Reset scroll position to top on page change
+	// 	const smoother = ScrollSmoother.get()
+	// 	if (smoother) {
+	// 		smoother.scrollTop(0)
+	// 	} else {
+	// 		window.scrollTo(0, 0)
+	// 	}
+	// }, [location.pathname])
 
 	return (
 		<div id="smooth-wrapper">
