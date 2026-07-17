eb38ba30fbc1664db659fba06eac80d6f313fce3
ryasan <ryansantos86@gmail.com>
2026-07-16 17:00:48 -0700

    Style: Enhance GSAP animations in blog and experience sections

--- app/components/sections/experience-section.tsx
+++ app/components/sections/experience-section.tsx
@@ -29,16 +29,18 @@ export default function ExperienceSection({
 
 			gsap.fromTo(
 				title,
-				{ opacity: 0 },
+				{ opacity: 0, y: 100 },
 				{
+					duration: 1,
 					ease: 'power2.out',
 					opacity: 1,
 					scrollTrigger: {
 						end: 'top center',
-						scrub: 1,
+						once: true,
 						start: 'top bottom-=150px',
 						trigger: title,
 					},
+					y: 0,
 				},
 			)
 
@@ -51,16 +53,18 @@ export default function ExperienceSection({
 			experienceItems.forEach((item) => {
 				gsap.fromTo(
 					item,
-					{ opacity: 0 },
+					{ opacity: 0, y: 100 },
 					{
+						duration: 1,
 						ease: 'power2.out',
 						opacity: 1,
 						scrollTrigger: {
 							end: 'top center',
-							scrub: 1,
+							once: true,
 							start: 'top bottom-=150px',
 							trigger: item,
 						},
+						y: 0,
 					},
 				)
 			})
@@ -115,7 +119,7 @@ export default function ExperienceSection({
 								const lastRect = lastNum.getBoundingClientRect()
 								const offset =
 									lastRect.top - containerRect.top + lastRect.height / 2
-								return `top+=${offset} center+=100px`
+								return `top+=${offset} center`
 							},
 							scrub: 1,
 							start: () => {
@@ -125,7 +129,7 @@ export default function ExperienceSection({
 								// Offset from container top
 								const offset =
 									firstRect.top - containerRect.top + firstRect.height / 2
-								return `top+=${offset} center+=100px`
+								return `top+=${offset} center`
 							},
 							trigger: container,
 						},
