9b4265aac6e95f1d4f467e95e22fe3ce9ca16d44
ryasan <ryansantos86@gmail.com>
2026-07-24 07:48:49 -0700

    Style: Enhance about section bio text

--- app/components/sections/about-section.tsx
+++ app/components/sections/about-section.tsx
@@ -130,10 +130,11 @@ export default function AboutSection({ id }: AboutSectionProps) {
 					<div className={`${ns}__bio-content`}>
 						<h3 className={`${ns}__name`}>Ryan Santos</h3>
 						<p className={`${ns}__bio-text`}>
-							I&apos;m a Frontend Engineer based out of Los Angeles, CA. I
-							specialize in building performant digital products with
-							human-centered usability at their core. My development career has
-							mostly been rooted in digital marketing and e-commerce.
+							I&apos;m a Frontend Engineer based in Los Angeles, CA. My craft is
+							building highly performant, accessible digital products with
+							human-centered usability at their core. I specialize in
+							transforming complex technical requirements into seamless,
+							user-first web experiences.
 						</p>
 						<ul className={`${ns}__socials`}>
 							{socialLinks.map((link) => {
